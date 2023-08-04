4b7cf69c07d52f62955999010dd4e00e55cea16c
Shubham0014 <shubham7744088156@gmail.com>
2023-08-04 23:35:27 +0530

    first commit

---
+++ src/main/java/resources/constants.java
@@ -0,0 +1,12 @@
+package resources;
+
+public class constants {
+
+	public static String username="Shubham";
+	public static String password="pass123";
+	public static String FirstName="Shubham";
+	public static String lastName="Patil";
+	public static String jobtitle="Globe India Enterprises";
+	
+
+}
